b578d9702005566a04a9aa0dd457f0f4c6753353
wuminmin <13355661100@189.cn>
2019-06-27 10:56:38 +0800

    111

--- django_twitter/views.py
+++ django_twitter/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render_to_response
 def index(request):
     try:
         print(request)
+        print('  ')
         return render_to_response("login.html")
     except:
         print(traceback.format_exc())
